c021facc7d7a5f325534b2bdd5e3f9e6f7f984b5
Jan Jablunka <jablunka@gmail.com>
2014-06-01 14:47:19 +0200

    incremental

--- src/MMB/HlasovaniZastupitelstva.php
+++ src/MMB/HlasovaniZastupitelstva.php
@@ -2,8 +2,10 @@
 
 namespace ZitBrno\Scrapers\MMB;
 
-class HlasovaniZastupitelstva extends \ZitBrno\Scrapers\Scraper; {
+class HlasovaniZastupitelstva extends \ZitBrno\Scrapers\Scraper {
 
-
+	static function scrape() {
+		var_dump(static::getURL('http://www.brno.cz/sprava-mesta/dokumenty-mesta/zapisy-ze-zastupitelstva-mesta-brna/'));
+	}
 
 }
--- src/Scraper.php
+++ src/Scraper.php
@@ -4,6 +4,16 @@ namespace ZitBrno\Scrapers;
 
 class Scraper {
 
+	static function getCacheDirectory() {
+		return \Katu\Utils\FS::joinPaths(TMP_PATH, implode('/', array_map('strtolower', array_slice(explode('\\', get_called_class()), 1))));
+	}
 
+	static function getURL($url, $timeout = NULL) {
+		$cacheOptions = array(
+			'dir' => static::getCacheDirectory(),
+		);
+
+		return \Katu\Utils\Cache::getURL($url, $timeout, $cacheOptions);
+	}
 
 }
