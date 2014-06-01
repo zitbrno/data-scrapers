e8090205ee4d3b75c5c1664edce361c0f0e01092
Jan Jablunka <jablunka@gmail.com>
2014-06-01 15:41:35 +0200

    incremental

--- src/MMB/ZapisyZeZastupitelstva/Hlasovani.php
+++ src/MMB/ZapisyZeZastupitelstva/Hlasovani.php
@@ -16,10 +16,16 @@ class Hlasovani extends \ZitBrno\Scrapers\Scraper {
 		return iconv('Windows-1250', 'UTF-8', parent::scrape($url, $timeout));
 	}
 
+	public function getCislo() {
+		$src = static::scrape($this->url);
+
+		preg_match('#<p class="header">.*Hlasování č. ([0-9]+)<br>.*</p>#sU', subject)
+	}
+
 	public function getHlasy() {
 		$src = static::scrape($this->url);
 
-		preg_match_all('#<td width="20%" align="right" nowrap style="font-size:8pt;font-family:Arial, Arial CE, Courier">(?<person>.+)\s+(?<party>.+): </td><td width="10%" align="left" nowrap style="font-size:8pt;font-family:Arial, Arial CE, Courier">(?<vote>.*)</td>#U', $src, $matches, PREG_SET_ORDER);
+		preg_match_all('#<td width="20%" align="right" nowrap style="font-size:8pt;font-family:Arial, Arial CE, Courier">(?<person>.+)\s+(?<party>.+): </td><td width="10%" align="left" nowrap style="font-size:8pt;font-family:Arial, Arial CE, Courier">(?<vote>.*)</td>#sU', $src, $matches, PREG_SET_ORDER);
 
 		$res = array();
 		foreach ($matches as $match) {
